42541f646643f3e8c4de417b9a059f6829796e8b
Daniel Iser <danieliser@wizardinternetsolutions.com>
2017-07-04 23:15:09 -0400

    First wave of updates to the boilerplate.

--- plugin-name/gulpfile.js
+++ plugin-name/gulpfile.js
@@ -1,80 +1,221 @@
-var gulp = require('gulp');
-var gulpLoadPlugins = require('gulp-load-plugins');
-var $ = gulpLoadPlugins();
+var gulp = require('gulp'),
+    runSequence = require('run-sequence').use(gulp),
+    $fn = require('gulp-load-plugins')({ camelize: true }),
+    plumberErrorHandler = {
+        errorHandler: $fn.notify.onError({
+            title: 'Gulp',
+            message: 'Error: <%= error.message %>'
+        })
+    },
+    pkg = require('./package.json');
 
-gulp.task('sass', function() {
-    return gulp.src('assets/sass/*.scss')
-        .pipe($.sass())
-        .pipe($.autoprefixer({
-            browsers: ['> 10%', 'last 2 versions'],
-            cascade: false
-        }))
-        .pipe($.csscomb())
-        .pipe(gulp.dest('assets/css'));
-});
-gulp.task('css', function() {
-    return gulp.src(['!assets/css/*.min.css', 'assets/css/*.css', ])
-        // Minify the CSS
-        .pipe($.csso())
-
-        // Rename the file with the .min.css extension
-        .pipe($.rename({ extname: '.min.css' }))
-
-        // Save the file
-        .pipe(gulp.dest('assets/css'))
-        .pipe($.livereload());
-});
-
+//region JavaScript
 gulp.task('js:admin', function() {
-    return gulp.src(['assets/js/src/admin/plugins/**/*.js', 'assets/js/src/admin/general.js'])
-        .pipe($.concat('popup-maker-admin.js'))
+    return gulp.src(['assets/js/src/admin/vendor/*.js', 'assets/js/src/admin/plugins/**/*.js', 'assets/js/src/admin/general.js'])
+        .pipe($fn.plumber(plumberErrorHandler))
+        .pipe($fn.jshint())
+        .pipe($fn.jshint.reporter('default'))
+        .pipe($fn.order([
+            "vendor/**/*.js",
+            "plugins/**/*.js",
+            'general.js'
+        ], { base: 'assets/js/src/admin/' }))
+        .pipe($fn.concat('admin.js'))
         .pipe(gulp.dest('assets/js'))
-        .pipe($.uglify())
-        .pipe($.rename({extname: '.min.js'}))
+        .pipe($fn.uglify())
+        .pipe($fn.rename({extname: '.min.js'}))
         .pipe(gulp.dest('assets/js'))
-        .pipe($.livereload());
+        .pipe($fn.livereload());
 });
+
 gulp.task('js:site', function() {
     return gulp.src(['assets/js/src/site/plugins/**/*.js', 'assets/js/src/site/general.js'])
-        .pipe($.concat('popup-maker-site.js'))
+        .pipe($fn.plumber(plumberErrorHandler))
+        .pipe($fn.jshint())
+        .pipe($fn.jshint.reporter('default'))
+        .pipe($fn.order([
+            "plugins/compatibility.js",
+            "plugins/pum.js",
+            "plugins/**/*.js",
+            'general.js'
+        ], { base: 'assets/js/src/site/' }))
+        .pipe($fn.concat('site.js'))
         .pipe(gulp.dest('assets/js'))
-        .pipe($.uglify())
-        .pipe($.rename({extname: '.min.js'}))
+        .pipe($fn.uglify())
+        .pipe($fn.rename({extname: '.min.js'}))
         .pipe(gulp.dest('assets/js'))
-        .pipe($.livereload());
+        .pipe($fn.livereload());
 });
+
 gulp.task('js:other', function() {
     return gulp.src('assets/js/src/*.js')
+        .pipe($fn.plumber(plumberErrorHandler))
+        .pipe($fn.jshint())
+        .pipe($fn.jshint.reporter('default'))
         .pipe(gulp.dest('assets/js'))
-        .pipe($.uglify())
-        .pipe($.rename({extname: '.min.js'}))
+        .pipe($fn.uglify())
+        .pipe($fn.rename({extname: '.min.js'}))
         .pipe(gulp.dest('assets/js'))
-        .pipe($.livereload());
+        .pipe($fn.livereload());
 });
 
+gulp.task('js', ['js:admin', 'js:site', 'js:other']);
+//endregion JavaScript
 
-gulp.task('js:complexity', function () {
-    return gulp.src('assets/js/src/*.js')
-        .pipe($.complexity());
+//region Language Files
+gulp.task('langpack', function () {
+    return gulp.src(['**/*.php', '!build/**/*.*'])
+        .pipe($fn.plumber(plumberErrorHandler))
+        .pipe($fn.sort())
+        .pipe($fn.wpPot( {
+            domain: pkg.name,
+            bugReport: 'https://wppopupmaker.com/support',
+            team: 'WP Popup Maker <support@wppopupmaker.com>'
+        } ))
+        .pipe(gulp.dest('languages'));
 });
-gulp.task('js:duplicates', function () {
-    return gulp.src('assets/js/src/*.js')
-        .pipe($.jscpd({
-            'min-lines': 10,
-            verbose    : true
-        }));
+//endregion Language Files
+
+//region SASS & CSS
+gulp.task('css', function() {
+    return gulp.src('assets/sass/*.scss')
+        .pipe($fn.plumber(plumberErrorHandler))
+        .pipe($fn.sourcemaps.init())
+        .pipe($fn.sass({
+            errLogToConsole: true,
+            outputStyle: 'expanded',
+            precision: 10
+        }))
+        .pipe($fn.sourcemaps.write())
+        .pipe($fn.sourcemaps.init({
+            loadMaps: true
+        }))
+        .pipe($fn.autoprefixer('last 2 version', '> 1%', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe($fn.sourcemaps.write('.'))
+        .pipe($fn.plumber.stop())
+        .pipe(gulp.dest('assets/css'))
+        .pipe($fn.filter('**/*.css')) // Filtering stream to only css files
+        .pipe($fn.combineMq()) // Combines Media Queries
+        .pipe($fn.livereload())
+        .pipe($fn.rename({ suffix: '.min' }))
+        .pipe($fn.csso({
+            //sourceMap: true,
+        }))
+        .pipe(gulp.dest('assets/css'))
+        .pipe($fn.livereload())
+        .pipe(gulp.dest('assets/css'));
 });
-gulp.task('js:lint', function () {
-    return gulp.src('assets/js/src/*.js')
-        .pipe($.jshint())
-        .pipe($.jshint.reporter('default'));
+//endregion SASS & CSS
+
+//region Cleaners
+gulp.task('clean-js:site', function () {
+    return gulp.src('assets/js/site*.js', {read: false})
+        .pipe($fn.plumber(plumberErrorHandler))
+        .pipe($fn.clean());
+});
+gulp.task('clean-js:admin', function () {
+    return gulp.src('assets/js/admin*.js', {read: false})
+        .pipe($fn.plumber(plumberErrorHandler))
+        .pipe($fn.clean());
+});
+gulp.task('clean-js:other', function () {
+    return gulp.src(['assets/js/*.js', '!assets/js/site*.js', '!assets/js/admin*.js'], {read: false})
+        .pipe($fn.plumber(plumberErrorHandler))
+        .pipe($fn.clean());
+});
+gulp.task('clean-css', function () {
+    return gulp.src(['assets/css/*.css', 'assets/css/*.css.map'], {read: false})
+        .pipe($fn.plumber(plumberErrorHandler))
+        .pipe($fn.clean());
+});
+gulp.task('clean-langpack', function () {
+    return gulp.src(['languages/*.pot'], {read: false})
+        .pipe($fn.plumber(plumberErrorHandler))
+        .pipe($fn.clean());
+});
+gulp.task('clean-build', function () {
+    return gulp.src('build/*', {read: false})
+        .pipe($fn.plumber(plumberErrorHandler))
+        .pipe($fn.clean());
+});
+gulp.task('clean-package', function () {
+    return gulp.src('release/'+pkg.name+'_v'+pkg.version+'.zip', {read: false})
+        .pipe($fn.plumber(plumberErrorHandler))
+        .pipe($fn.clean({force: true}));
 });
 
-gulp.task('watch', function() {
-    $.livereload.listen();
-    gulp.watch('assets/sass/**/*.scss', ['sass']);
-    gulp.watch('assets/css/*.css', ['css']);
+// Cleaning Routines
+gulp.task('clean-js', function (done) {
+    runSequence(
+        ['clean-js:site', 'clean-js:admin', 'clean-js:other'],
+        done
+    );
+});
+gulp.task('clean-all', function (done) {
+    runSequence(
+        ['clean-js', 'clean-css', 'clean-langpack'],
+        ['clean-build', 'clean-package'],
+        done
+    );
+});
+//endregion Cleaners
+
+//region Watch & Build
+gulp.task('watch', function () {
+    $fn.livereload.listen();
+    gulp.watch('assets/sass/**/*.scss', ['css']);
     gulp.watch('assets/js/src/admin/**/*.js', ['js:admin']);
     gulp.watch('assets/js/src/site/**/*.js', ['js:site']);
-    gulp.watch(['assets/js/src/**/*.js', '!assets/js/src/admin/**/*.js', '!assets/js/src/admin/**/*.js'], ['js:other']);
+    gulp.watch(['assets/js/src/**/*.js', '!assets/js/src/site/**/*.js', '!assets/js/src/admin/**/*.js'], ['js:other']);
+    gulp.watch('**/*.php', ['langpack']);
+});
+
+// Cleans & Rebuilds Assets Prior to Builds
+gulp.task('prebuild', function (done) {
+    runSequence(
+        'clean-all',
+        ['css', 'js', 'langpack'],
+        done
+    );
+});
+
+// Copies a clean set of build files into the build folder
+gulp.task('build', ['prebuild'], function () {
+    return gulp.src(['./**/*.*', '!./build/**', '!./release/**', '!./node_modules/**', '!./gulpfile.js', '!./package.json', '!./assets/js/src/**'])
+        .pipe($fn.plumber(plumberErrorHandler))
+        .pipe(gulp.dest('build/'+pkg.name));
+});
+
+// Generates a release package with the current version from package.json
+gulp.task('package', ['clean-package'], function () {
+    return gulp.src('build/**/*.*')
+        .pipe($fn.plumber(plumberErrorHandler))
+        .pipe($fn.zip(pkg.name+'_v'+pkg.version+'.zip'))
+        .pipe(gulp.dest('release'));
+});
+
+// Runs all build routines and generates a release.
+gulp.task('release', function (done) {
+    runSequence(
+        'build',
+        'package',
+        done
+    );
+});
+
+// Runs a release and cleans up afterwards.
+gulp.task('release:clean', ['release'], function (done) {
+    runSequence(
+        'clean-build',
+        done
+    );
+});
+//endregion Watch & Build
+
+gulp.task('default', function (done) {
+    runSequence(
+        'prebuild',
+        'watch',
+        done
+    );
 });
--- plugin-name/plugin-name.php
+++ plugin-name/plugin-name.php
@@ -45,73 +45,117 @@
 
 
 // Exit if accessed directly
-if( !defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-if( !class_exists( 'PUM_@@classname' ) ) {
+
+/**
+ * Class Autoloader
+ *
+ * @param $class
+ */
+function pum_@@prefix_autoloader( $class ){
+
+	// project-specific namespace prefix
+	$prefix = 'PUM_@@constant_';
+
+	// base directory for the namespace prefix
+	$base_dir = __DIR__ . '/classes/';
+
+	// does the class use the namespace prefix?
+	$len = strlen( $prefix );
+	if ( strncmp( $prefix, $class, $len ) !== 0 ) {
+		// no, move to the next registered autoloader
+		return;
+	}
+
+	// get the relative class name
+	$relative_class = substr( $class, $len );
+
+	// replace the namespace prefix with the base directory, replace namespace
+	// separators with directory separators in the relative class name, append
+	// with .php
+	$file = $base_dir . str_replace( '_', '/', $relative_class ) . '.php';
+
+	// if the file exists, require it
+	if ( file_exists( $file ) ) {
+		require_once $file;
+	}
+
+}
+
+if ( ! function_exists( 'spl_autoload_register' ) ) {
+	include 'includes/compat.php';
+}
+
+spl_autoload_register( 'pum_@@prefix_autoloader' ); // Register autoloader
+
 
 	/**
 	 * Main PUM_@@classname class
-	 *
-	 * @since       1.0.0
 	 */
 	class PUM_@@classname {
 
-		/**
-		 * @var         PUM_@@classname $instance The one true PUM_@@classname
-		 * @since       1.0.0
-		 */
-		private static $instance;
+	/**
+	 * @var         PUM_@@classname $instance The one true PUM_@@classname
+	 */
+	private
+	static $instance;
 
 
-		/**
-		 * Get active instance
-		 *
-		 * @access      public
-		 * @since       1.0.0
-		 * @return      object self::$instance The one true PUM_@@classname
-		 */
-		public static function instance() {
-			if( !self::$instance ) {
-				self::$instance = new PUM_@@classname();
+	/**
+	 * Get active instance
+	 *
+	 * @access      public
+	 * @since       1.0.0
+	 * @return      object self::$instance The one true PUM_@@classname
+	 */
+	public
+	static function instance() {
+		if ( ! self::$instance ) {
+			self::$instance = new PUM_@@classname();
 				self::$instance->setup_constants();
 				self::$instance->includes();
 				self::$instance->load_textdomain();
 				self::$instance->hooks();
 			}
 
-			return self::$instance;
-		}
+		return self::$instance;
+	}
 
 
-		/**
-		 * Setup plugin constants
-		 *
-		 * @access      private
-		 * @since       1.0.0
-		 * @return      void
-		 */
-		private function setup_constants() {
-			// Plugin version
-			define( 'PUM_@@constant_VER', '1.0.0' );
+	/**
+	 * Setup plugin constants
+	 *
+	 * @access      private
+	 * @since       1.0.0
+	 * @return      void
+	 */
+	private
+	function setup_constants() {
+		// Plugin version
+		define( 'PUM_@@constant_VER', '1.0.0' );
 
-			// Plugin path
-			define( 'PUM_@@constant_DIR', plugin_dir_path( __FILE__ ) );
+		// Plugin path
+		define( 'PUM_@@constant_DIR', plugin_dir_path( __FILE__ ) );
 
-			// Plugin URL
-			define( 'PUM_@@constant_URL', plugin_dir_url( __FILE__ ) );
-		}
+		// Plugin URL
+		define( 'PUM_@@constant_URL', plugin_dir_url( __FILE__ ) );
+	}
 
 
-		/**
-		 * Include necessary files
-		 *
-		 * @access      private
-		 * @since       1.0.0
-		 * @return      void
-		 */
-		private function includes() {
-			// Include scripts
-			require_once PUM_@@constant_DIR . 'includes/scripts.php';
+	/**
+	 * Include necessary files
+	 *
+	 * @access      private
+	 * @since       1.0.0
+	 * @return      void
+	 */
+	private
+	function includes() {
+		// Include scripts
+		require_once PUM_@@constant_DIR . 'includes/scripts.php';
 			require_once PUM_@@constant_DIR . 'includes/functions.php';
 
 			/**
@@ -125,50 +169,52 @@ if( !class_exists( 'PUM_@@classname' ) ) {
 		}
 
 
-		/**
-		 * Run action and filter hooks
-		 *
-		 * @access      private
-		 * @since       1.0.0
-		 * @return      void
-		 *
-		 * @todo        The hooks listed in this section are a guideline, and
-		 *              may or may not be relevant to your particular extension.
-		 *              Please remove any unnecessary lines, and refer to the
-		 *              WordPress codex and Popup Maker documentation for additional
-		 *              information on the included hooks.
-		 *
-		 *              This method should be used to add any filters or actions
-		 *              that are necessary to the core of your extension only.
-		 *              Hooks that are relevant to meta boxes, widgets and
-		 *              the like can be placed in their respective files.
-		 *
-		 *              IMPORTANT! If you are releasing your extension as a
-		 *              commercial extension in the Popup Maker store, DO NOT remove
-		 *              the license check!
-		 */
-		private function hooks() {
-			// Register settings
-			add_filter( 'popmake_settings_extensions', array( $this, 'settings' ), 1 );
+	/**
+	 * Run action and filter hooks
+	 *
+	 * @access      private
+	 * @since       1.0.0
+	 * @return      void
+	 *
+	 * @todo        The hooks listed in this section are a guideline, and
+	 *              may or may not be relevant to your particular extension.
+	 *              Please remove any unnecessary lines, and refer to the
+	 *              WordPress codex and Popup Maker documentation for additional
+	 *              information on the included hooks.
+	 *
+	 *              This method should be used to add any filters or actions
+	 *              that are necessary to the core of your extension only.
+	 *              Hooks that are relevant to meta boxes, widgets and
+	 *              the like can be placed in their respective files.
+	 *
+	 *              IMPORTANT! If you are releasing your extension as a
+	 *              commercial extension in the Popup Maker store, DO NOT remove
+	 *              the license check!
+	 */
+	private
+	function hooks() {
+		// Register settings
+		add_filter( 'popmake_settings_extensions', array( $this, 'settings' ), 1 );
 
-			// Handle licensing
-			// @todo        Replace the Plugin Name and Your Name with your data
-			if( class_exists( 'PUM_License' ) ) {
-				$license = new PUM_License( __FILE__, '@@name', PUM_@@constant_VER, '@@author' );
+		// Handle licensing
+		// @todo        Replace the Plugin Name and Your Name with your data
+		if ( class_exists( 'PUM_License' ) ) {
+			$license = new PUM_License( __FILE__, '@@name', PUM_@@constant_VER, '@@author' );
 			}
-		}
+	}
 
 
-		/**
-		 * Internationalization
-		 *
-		 * @access      public
-		 * @since       1.0.0
-		 * @return      void
-		 */
-		public function load_textdomain() {
-			// Set filter for language directory
-			$lang_dir = PUM_@@constant_DIR . '/languages/';
+	/**
+	 * Internationalization
+	 *
+	 * @access      public
+	 * @since       1.0.0
+	 * @return      void
+	 */
+	public
+	function load_textdomain() {
+		// Set filter for language directory
+		$lang_dir = PUM_@@constant_DIR . '/languages/';
 			$lang_dir = apply_filters( 'pum_@@prefix_languages_directory', $lang_dir );
 
 			// Traditional WordPress plugin locale filter
@@ -176,13 +222,13 @@ if( !class_exists( 'PUM_@@classname' ) ) {
 			$mofile = sprintf( '%1$s-%2$s.mo', '@@text_domain', $locale );
 
 			// Setup paths to current locale file
-			$mofile_local   = $lang_dir . $mofile;
-			$mofile_global  = WP_LANG_DIR . '/popmake-plugin-name/' . $mofile;
+			$mofile_local  = $lang_dir . $mofile;
+			$mofile_global = WP_LANG_DIR . '/popmake-plugin-name/' . $mofile;
 
-			if( file_exists( $mofile_global ) ) {
+			if ( file_exists( $mofile_global ) ) {
 				// Look in global /wp-content/languages/popmake-plugin-name/ folder
 				load_textdomain( '@@text_domain', $mofile_global );
-			} elseif( file_exists( $mofile_local ) ) {
+			} elseif ( file_exists( $mofile_local ) ) {
 				// Look in local /wp-content/plugins/popmake-plugin-name/languages/ folder
 				load_textdomain( '@@text_domain', $mofile_local );
 			} else {
@@ -192,29 +238,30 @@ if( !class_exists( 'PUM_@@classname' ) ) {
 		}
 
 
-		/**
-		 * Add settings
-		 *
-		 * @access      public
-		 * @since       1.0.0
-		 * @param       array $settings The existing Popup Maker settings array
-		 * @return      array The modified Popup Maker settings array
-		 */
-		public function settings( $settings ) {
-			$new_settings = array(
-				array(
-					'id'    => 'pum_@@prefix_settings',
-					'name'  => '<strong>' . __( '@@name Settings', '@@text_domain' ) . '</strong>',
-					'desc'  => __( 'Configure @@name Settings', '@@text_domain' ),
-					'type'  => 'header',
-				)
-			);
+	/**
+	 * Add settings
+	 *
+	 * @access      public
+	 * @since       1.0.0
+	 *
+	 * @param       array $settings The existing Popup Maker settings array
+	 *
+	 * @return      array The modified Popup Maker settings array
+	 */
+	public
+	function settings( $settings ) {
+		$new_settings = array(
+			array(
+				'id'   => 'pum_@@prefix_settings',
+				'name' => '<strong>' . __( '@@name Settings', '@@text_domain' ) . '</strong>',
+				'desc' => __( 'Configure @@name Settings', '@@text_domain' ),
+				'type' => 'header',
+			),
+		);
 
-			return array_merge( $settings, $new_settings );
-		}
+		return array_merge( $settings, $new_settings );
 	}
-} // End if class_exists check
-
+}
 
 /**
  * The main function responsible for returning the one true PUM_@@classname
@@ -228,9 +275,9 @@ if( !class_exists( 'PUM_@@classname' ) ) {
  *              situations where your extension is activated but Popup Maker is not
  *              present.
  */
-function pum_@@prefix_load() {
-	if( ! class_exists( 'Popup_Maker' ) ) {
-		if( ! class_exists( 'PUM_Extension_Activation' ) ) {
+function pum_@@prefix_load(){
+	if ( ! class_exists( 'Popup_Maker' ) ) {
+		if ( ! class_exists( 'PUM_Extension_Activation' ) ) {
 			require_once 'includes/class-extension-activation.php';
 		}
 
@@ -252,7 +299,5 @@ add_action( 'plugins_loaded', 'pum_@@prefix_load' );
  * @since       1.0.0
  * @return      void
  */
-function pum_@@prefix_activation() {
-	/* Activation functions here */
-}
+function pum_@@prefix_activation(){/* Activation functions here */}
 register_activation_hook( __FILE__, 'pum_@@prefix_activation' );
